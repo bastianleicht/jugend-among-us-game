51f5010b53600a2bcfa879d9638692f6edb709d7
Bastian Leicht <mail@bastianleicht.de>
2021-07-30 16:36:08 +0200

    Game: Implemented toastr & added Connection Messages
    This now displays a error message if the Player is not connected to the Socket. It also tells him, when he connected again.

--- src/public/game.js
+++ src/public/game.js
@@ -64,6 +64,32 @@ const socket = io({
 	}
 });
 
+toastr.options = {
+	"closeButton": false,
+	"debug": false,
+	"newestOnTop": true,
+	"progressBar": false,
+	"positionClass": "toast-bottom-center",
+	"preventDuplicates": false,
+	"onclick": null,
+	"showDuration": "300",
+	"hideDuration": "1000",
+	"timeOut": "5000",
+	"extendedTimeOut": "1000",
+	"showEasing": "swing",
+	"hideEasing": "linear",
+	"showMethod": "fadeIn",
+	"hideMethod": "fadeOut"
+}
+
+socket.on('disconnect', () => {
+	toastr.warning('Disconnected!');
+});
+
+socket.on('connect', () => {
+	toastr.success('Connected!');
+})
+
 log(`Got customName: ${save_customName$}`);
 log(`Got customID: ${save_customID$}`);
 
